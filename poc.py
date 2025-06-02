6796e7690ed94ef4bea9176f8425a239edd8a54a
M. Greyson Christoforo <grey@christoforo.net>
2025-06-02 02:29:19 -0600

    Create poc.py

---
+++ poc.py
@@ -0,0 +1,80 @@
+import serial
+import struct
+
+class R3P(serial.Serial):
+    """
+    River 3 Plus comms from scratch via USB CDC (ACM)
+    """
+    base_len = 20
+    header_len = 4
+    header = 0x03aa
+    overhead_len = 14
+
+    def __init__(self, comport:str):
+        comms_args = {
+            "port": None,
+            "baudrate": 115200,
+            "bytesize": serial.EIGHTBITS,
+            "parity": serial.PARITY_NONE,
+            "stopbits": serial.STOPBITS_ONE,
+            }
+        super().__init__(**comms_args)
+        self.port = comport
+
+    def __enter__(self):
+        return super().__enter__()
+    def __exit__(self, type, value, traceback):
+        return super().__exit__(type, value, traceback)
+
+    @staticmethod
+    def crc16(data : bytearray):
+        """
+        CRC-16/ARC
+        """
+        offset = 0
+        length = len(data)
+        crc = 0x0000
+        for i in range(length):
+            crc ^= data[offset + i]
+            for j in range(8):
+                if ((crc & 0x1) == 1):
+                    crc = int((crc / 2)) ^ 40961
+                else:
+                    crc = int(crc / 2)
+        return crc & 0xFFFF
+
+    def tx(self, msg: str):
+        bmsg = bytes.fromhex(msg)
+        msg_len = len(bmsg) - self.overhead_len
+        headmsg = struct.pack('<HH', self.header, msg_len) + bmsg
+        crc_val = R3P.crc16(headmsg)
+        out = headmsg + struct.pack('<H', crc_val)
+        #print(f'>>> {bmsg.hex()}')
+        return self.write(out)
+
+    def rx(self):
+        header = self.read(self.header_len)
+        preamble, var_len = struct.unpack('<HH', header)
+        payload = self.read(self.base_len + var_len - self.header_len - 2)
+        crc = self.read(2)
+        if R3P.crc16(header + payload + crc) == 0:
+            pass
+            #print(f'<<< {payload.hex()}')
+        else:
+            raise RuntimeError("CRC check fail")
+        return payload
+            
+    def query(self, msg: str):
+        self.tx(msg)
+        return self.rx()
+
+    def get_serial(self):
+        serial_payload = self.query("f40d00000000ffff2202010166031600")
+        rheader, serial = struct.unpack('18s16s', serial_payload)
+        return serial.decode()
+
+
+if __name__ == "__main__":
+    serial_port = "COM3"
+    with R3P(serial_port) as r:
+        print(f"{r.get_serial()=}")
